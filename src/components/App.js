3489b7e61293b3775e47a129a6d129828a9fa49c
aschemily <aschemily@gmail.com>
2019-02-02 21:11:44 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,77 @@ class App extends React.Component {
       }
     }
   }
+   componentDidMount(){
+    fetch('/api/pets')
+    .then(r => r.json())
+    .then(pets => {this.setState({pets})})
+      // let petz = pets.map((pet)=>{
+      //   return(
+      //     //console.log(pet)
+      //
+      //     <div key={pet.id}>
+      //       <h2>{pet.name}</h2>
+      //       <p>{pet.age}</p>
+      //       <p>{pet.gender}</p>
+      //       <p>{pet.type}</p>
+      //       <p>{pet.weight}</p>
+      //       <p>{pet.isAdopted.toString()}</p>
+      //     </div>
+      //   )
+      // })
+      //console.log("state",this.state.pets)
+      //console.log(data)
+
+  }
+
+  onChangeType = (event)=>{
+  ///console.log('selecting from app event',event.target.value)
+    if(event.target.value === 'cat'){
+      //console.log('clicking cat')
+      fetch('/api/pets?type=cat')
+      .then(r => r.json())
+      .then(cats => {this.setState({pets:cats})})
+    }  else if (event.target.value === 'dog'){
+      fetch('/api/pets?type=dog')
+      .then(r => r.json())
+      .then(dogs => {this.setState({pets:dogs})})
+    } else if (event.target.value === 'micropig'){
+      fetch('/api/pets?type=micropig')
+      .then(r => r.json())
+      .then(pigs => {this.setState({pets:pigs})})
+    }  else if (event.target.value === 'all'){
+      fetch('/api/pets')
+      .then(r => r.json())
+      .then(pets => {this.setState({pets})})
+    }
+  }
+
+  onFindPetsClick = (event)=>{
+    console.log('clicking from app onFindPetsClick event', event)
+  }
+
+  onAdoptPet = petId =>{
+    //console.log(this.state.pets.isAdopted)
+    const pets = this.state.pets.map(pet=>{
+
+    //  return pet.id === petId ? {...pet, isAdopted:true} : pet
+      // console.log(pet.isAdopted)
+      // console.log(event.target.value)
+      if(pet.id === petId){
+         return {...pet, isAdopted: true}
+      } else {
+        return pet
+      }
+    })
+    this.setState({pets},()=>console.log('new state',this.state))
+    //console.log('clicking onAdoptPet event in appt',event.target.value)
+  }
+
 
   render() {
+    //console.log(this.state.filters.type)
+    //console.log(this.state.pets)
+    //console.log('app state',this.state.pets)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +93,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}
+                petsFilter={this.state.filters.type}
+                onFindPetsClick={this.onFindPetsClick}
+                />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}
+                onAdoptPet={this.onAdoptPet}
+                />
+
             </div>
           </div>
         </div>
@@ -35,5 +110,6 @@ class App extends React.Component {
     )
   }
 }
+
 
 export default App
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,26 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    //const petsData = console.
+    //console.log('pet browser',this.props)
+    //console.log('pet browser props are:',this.props.pets)
+    return (
+      <div className="ui cards">
+        {this.props.pets.map(pet=>{
+          //console.log(pet)
+          return (
+            <Pet
+              key={pet.id}
+              pet={pet}
+              onAdoptPet={this.props.onAdoptPet}
+              />
+          )
+        })}
+     </div>
+   )
+
   }
 }
 
